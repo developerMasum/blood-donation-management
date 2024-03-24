3b3af2b3443cd89c056c9c68e394acc8dbbae926
developerMasum <developer.masum2001@gmail.com>
2024-03-24 22:30:37 +0600

    blood donation Client

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@nextui-org/react";
+
+
+const LogInPage = () => {
+    return (
+        <div>
+            this is login page 
+            <Button> click me </Button>
+        </div>
+    );
+};
+
+export default LogInPage;
---
+++ src/app/test/page.tsx
@@ -0,0 +1,16 @@
+import { motion } from "framer-motion"
+
+const TestPage = () => {
+  return (
+    <div>
+      <motion.div
+    initial={{ opacity: 0, scale: 0.5 }}
+    animate={{ opacity: 1, scale: 1 }}
+    transition={{ duration: 0.5 }}
+  >
+  </motion.div>
+    </div>
+  );
+};
+
+export default TestPage;
