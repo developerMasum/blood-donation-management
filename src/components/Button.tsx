74d6806c5ac3c25e351c41d4db7214ea603ecafd
saifulaija <saifulaija2023@gmail.com>
2024-03-26 13:38:57 +0600

    ok

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ import { Button } from "@nextui-org/button";
 export default function Page() {
   return (
     <div>
-      <Button>Click me</Button>
+      <Button>Click me here</Button>
     </div>
   );
 }
